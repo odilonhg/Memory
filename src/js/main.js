c08b014d90a58cd74113b8508e528bab614aef4b
Dylan MESNAGE <dmesnage.hg@gmail.com>
2026-09-23 11:57:08 +0200

    :sparkles: complete card click handling, dynamic image rendering, and asynchronous match validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 let dimension = 150;
 let imgStart = 1 + Math.floor(Math.random() * 99);
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let moves = 0;
+let matchedCount = 0;
 const uniqueIds = new Set();
 
 while (uniqueIds.size < 8) {
@@ -21,19 +26,74 @@ function shuffle(array) {
     return array;
 }
 
+
+/**
+ * Initialize the game
+ */
 function initGame() {
     const board = document.querySelector("#game-board");
     ids = shuffle(ids);
 
     ids.forEach((id) => {
         const card = document.createElement("div");
-        card.className = "card";
+        card.classList = "card";
         card.dataset.value = `https://picsum.photos/id/${id}/${dimension}`;
         card.style.width = `${dimension}px`; card.style.height = `${dimension}px`;
         card.setAttribute("role", "button");
         card.setAttribute("tabindex", "0");
+        card.addEventListener("click", () => handleCardClick(card));
         board.appendChild(card);
     });
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} card - Card clicked
+ */
+function handleCardClick(card) {
+    if (card.classList.contains("flipped") || card.classList.contains("matched")) return;
+
+    card.classList.add("flipped");
+
+    if (!firstCard) {
+        firstCard = card;
+        card.innerHTML = `<img src="${card.dataset.value}" alt="Image ${card.dataset.value}" />`
+    } else {
+        secondCard = card;
+        card.innerHTML = `<img src="${card.dataset.value}" alt="Image ${card.dataset.value}" />`
+        lockBoard = true;
+        moves++;
+        checkMatch();
+    }
+}
+
+/**
+ * Check if the two cards match
+ */
+function checkMatch() {
+    if (firstCard.dataset.value === secondCard.dataset.value) {
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+        matchedCount++;
+        resetBoard();
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove("flipped");
+            firstCard.innerHTML = "";
+            secondCard.classList.remove("flipped");
+            secondCard.innerHTML = "";
+            resetBoard();
+        }, 800);
+    }
+}
+
+/**
+ * Reset the board
+ */
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
 }
 
 initGame();
